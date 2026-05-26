53cac42be83940c6ccf3afde88c1fda02e51a43e
Eric <ericyuanovo@gmail.com>
2026-05-26 18:58:18 +0800

    feat: add useful ts util type

--- src/types.ts
+++ src/types.ts
@@ -9,3 +9,15 @@ export type ExtractInner<T> = T extends (...args: any[]) => infer R
       : T extends ComputedRef<infer V>
         ? ExtractInner<V>
         : T
+
+/**
+ * 提取常量对象的联合类型
+ */
+export type EnumKeys<T> = keyof T
+export type EnumValues<T> = T[keyof T]
+
+/**
+ * 字段必填/可选控制
+ */
+export type MakeRequired<T, K extends keyof T> = Partial<Omit<T, K>> & Required<Pick<T, K>>
+export type MakeOptional<T, K extends keyof T> = Required<Omit<T, K>> & Partial<Pick<T, K>>
